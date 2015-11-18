d4bd716a7e1ff6554d2af0d826d5bf75f3d43de8
antebete <antebete@192.168.0.115>
2015-11-17 19:31:38 -0500

    MY FIRST GIT

---
+++ z/src/z/Z.java
@@ -0,0 +1,17 @@
+package z;
+
+public class Z {
+	
+	void calculate(int a, int b){System.out.print(a + b );}
+	void calculate(double c, double d){System.out.print(c*d);}
+	
+ public static void main(String[] arr){
+	
+	Z z= new Z();
+	
+	z.calculate(5, 6);
+
+}
+	
+
+}
