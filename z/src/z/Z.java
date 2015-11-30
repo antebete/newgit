467fff75dd32e048a642d4710d33cc163e5d00f2
antebete <antebete@192.168.0.116>
2015-11-29 19:13:46 -0500

    commit

--- z/src/z/Z.java
+++ z/src/z/Z.java
@@ -2,14 +2,15 @@ package z;
 
 public class Z {
 	
-	void calculate(int a, int b){System.out.print(a + b );}
+	void calculate(int a, int b){System.out.println(a + b );}
 	void calculate(double c, double d){System.out.print(c*d);}
 	
  public static void main(String[] arr){
 	
 	Z z= new Z();
 	
-	z.calculate(5, 6);
+	z.calculate(5.5, 4.5);
+	
 
 }
 	
